c54ec18dc37d52a0681f81730292162b9a6ec771
agarwalsankalp <agarwalsankalp@gmail.com>
2011-01-26 22:33:42 +0530

    xml encode issue

--- app/controllers/handler_controller.rb
+++ app/controllers/handler_controller.rb
@@ -15,21 +15,23 @@ class HandlerController < ApplicationController
     logger.debug("Before encode: "+uri_string)
     
     @return_xml=mediator(uri_string)
+    require 'rexml/document'
+    doc = REXML::Document.new(CGI.unescapeHTML(@return_xml))
+    @return_xml=doc.elements[1].elements[1].to_s
     
     cxml_uri="http://cxml.lfn.net/internalposts.cxml?xml="+@return_xml
     
     begin
       encoded_uri_string=URI.encode(cxml_uri)
-      logger.debug("After encode cxml uri: "+encoded_uri_string)
+      logger.debug("Before encode cxml uri: "+encoded_uri_string)
       uri = URI.parse(encoded_uri_string)
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new(uri.path)
       response = http.request(request)
-      logger.debug(response.body)
     rescue
     end
     
-    @return_body=response.body
+    logger.debug(response.body)
     
   end
   
@@ -37,7 +39,7 @@ class HandlerController < ApplicationController
     require 'net/http'
     require 'uri'
     begin
-      encoded_uri_string=URI.encode(uri_string).gsub("%","!")
+      encoded_uri_string=uri_string 
       logger.debug("After encode: "+encoded_uri_string)
       uri = URI.parse(encoded_uri_string)
       http = Net::HTTP.new(uri.host, uri.port)
