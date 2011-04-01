632ba80e4ce4e536c4bf3caf9a61b1165cf87a77
jrichardlai <jrichardlai@gmail.com>
2011-03-31 23:16:14 -0700

    Fixed typo.

--- app/third_party_services/model/lfn.rb
+++ app/third_party_services/model/lfn.rb
@@ -36,7 +36,7 @@ class Lfn < ThirdPartyService
       params            = {
        :userId          => user.person.diaspora_handle.to_s,
        :aspectIds       => value_as_array(params[:target_aspect_ids]),
-       :aspectContacts  => get_aspect_contacts_from_ids(target_aspect_ids, user),
+       :aspectContacts  => get_aspect_contacts_from_ids(params[:target_aspect_ids], user),
        :message         => message,
        :postControl     => params[:post].control.try(:to_json),
        :postUrl         => image_url
