744785dc630b95adec7806a1e4e9df40f7d7267c
jrichardlai <jrichardlai@gmail.com>
2011-04-02 00:32:13 -0700

    Added debug to invoke method.

--- app/third_party_services/model/third_party_service.rb
+++ app/third_party_services/model/third_party_service.rb
@@ -74,8 +74,13 @@ class ThirdPartyService
     # TODO change the verb
     response = Net::HTTP.post_form(URI.parse(service_url), {:method => method, :params => encrypted_values})
 
+    Rails.logger.info("Response before decrypt : #{CGI::unescapeHTML(response.body)}")
+
     doc = Document.new(response.body)
-    doc.each_element('//Column') { |column| column.text = AESCrypt.decrypt(Base64.decode64(column.text, AppConfig[:encryption_key], AppConfig[:iv], "AES-256-CBC")) }
+
+    doc.each_element('//Column') { |column| column.text = AESCrypt.decrypt(Base64.decode64(column.text.to_s), AppConfig[:encryption_key], AppConfig[:iv], "AES-256-CBC") }
+
+    Rails.logger.info("Response after decrypt : #{CGI::unescapeHTML(doc.to_s)}")
 
     doc.to_s
   end
